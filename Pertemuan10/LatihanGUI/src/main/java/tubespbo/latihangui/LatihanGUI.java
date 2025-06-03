7419f130f8ba6b99e562181959bcd5f7867a2175
gi2br3n1906 <67181199+gi2br3n1906@users.noreply.github.com>
2025-06-03 21:19:57 +0700

    ini juga kemaren dah ngumpulin malah salah repo

---
+++ Pertemuan10/LatihanGUI/src/main/java/tubespbo/latihangui/LatihanGUI.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package tubespbo.latihangui;
+
+/**
+ *
+ * @author User
+ */
+public class LatihanGUI {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
