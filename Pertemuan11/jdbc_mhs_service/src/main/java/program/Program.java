8e7a91215a6c67a7b97ebfd284165dcecca45cdb
gi2br3n1906 <67181199+gi2br3n1906@users.noreply.github.com>
2025-06-04 00:03:31 +0700

    upl

---
+++ Pertemuan11/jdbc_mhs_service/src/main/java/program/Program.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package program;
+
+import java.util.*;
+import model.Mahasiswa;
+import service.MysqlMahasiswaService;
+
+/**
+ *
+ * @author User
+ */
+public class Program {
+    static MysqlMahasiswaService service = new MysqlMahasiswaService();
+    
+    public static void displayAll(){
+        List<Mahasiswa> listmhs = service.getAll();
+        System.out.println("===list mahasiswa===");
+        for (Mahasiswa mhs : listmhs) {
+            System.out.println(mhs);
+        }
+    }
+    public static void main(String[] args) {
+        List<Mahasiswa> listmhs = new ArrayList<>();
+        System.out.println("");
+        
+        // insert
+        System.out.println("===insert");
+        Mahasiswa mhsAdd = new Mahasiswa(5, "Haryo");
+        service.add(mhsAdd);
+        System.out.println("berhasil insert: " + mhsAdd);
+        displayAll();
+        
+        // update
+        System.out.println("===update");
+        Mahasiswa mhsUpdate = service.getById(1);
+        System.out.println("data sebelum update: " + mhsUpdate);
+        mhsUpdate.setNama("Haryo Wibowo");
+        service.update(mhsUpdate);
+        System.out.println("berhasil update: " + mhsUpdate);
+        displayAll();
+
+        //delete
+        System.out.println("===delete");
+        System.out.println("data sebelum delete: " + mhsUpdate);
+        service.delete(mhsUpdate.getId());
+        System.out.println("berhasil delete: " + mhsUpdate);
+        displayAll();
+        
+    }
+}
---
+++ Pertemuan11/jdbc_mhs_service/src/main/java/service/MysqlMahasiswaService.java
@@ -0,0 +1,101 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package service;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Mahasiswa;
+import utilities.MysqlUtility;
+
+/**
+ *
+ * @author User
+ */
+
+
+public class MysqlMahasiswaService {
+    Connection koneksi = null;
+    
+    public MysqlMahasiswaService(){
+        koneksi = MysqlUtility.getConnection();
+    }
+    
+    public Mahasiswa makeMhsObject(ResultSet rs) throws SQLException {
+        int id = rs.getInt("id");
+        String nama = rs.getString("nama");
+        return new Mahasiswa(id, nama);
+    }
+    
+    public void add(Mahasiswa mhs) {
+        String sql = "INSERT INTO mahasiswa (nama) VALUES (?)";
+        try {
+            PreparedStatement ps = koneksi.prepareStatement(sql);
+            ps.setString(1, mhs.getNama());
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void update(Mahasiswa mhs) {
+        String sql = "UPDATE mahasiswa SET nama = ? WHERE id = ?";
+        try {
+            PreparedStatement ps = koneksi.prepareStatement(sql);
+            ps.setString(1, mhs.getNama());
+            ps.setInt(2, mhs.getId());
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void delete(int id) {
+        String sql = "DELETE FROM mahasiswa WHERE id = ?";
+        try {
+            PreparedStatement ps = koneksi.prepareStatement(sql);
+            ps.setInt(1, id);
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Mahasiswa getById(int id) {
+        String sql = "SELECT * FROM mahasiswa WHERE id = ?";
+        Mahasiswa mhs = null;
+        try {
+            PreparedStatement ps = koneksi.prepareStatement(sql);
+            ps.setInt(1, id);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                mhs = makeMhsObject(rs);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();   
+        }
+        return mhs;
+    }
+
+    public List<Mahasiswa> getAll() {
+        String sql = "SELECT * FROM mahasiswa";
+        List<Mahasiswa> listMhs = new ArrayList<>();
+        try {
+            Statement stmt = koneksi.createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
+            while (rs.next()) {
+                listMhs.add(makeMhsObject(rs));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return listMhs;
+    }
+}
